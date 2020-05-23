afa699b7f44f399323b8de5ae7ca8abd2109201c
Anuj Pancholi <anuj.pancholi.1@gmail.com>
2020-05-23 20:08:14 +0530

    refactor: introduce Part component and refactor Courses to import Part

--- part1/src/components/Courses.js
+++ part1/src/components/Courses.js
@@ -1,10 +1,10 @@
 import React from 'react';
-
+import Part from "./Part.js";
 
 
 const Courses = (props) => {
 
-  let partsJsx = props.parts.map(part => (<p>{part.name} {part.exerciseCount}</p>)).reduce((acc,curr) => [acc,curr]);
+  let partsJsx = props.parts.map(part => (<Part name={part.name} excerciseCount={part.excerciseCount}/>)).reduce((acc,curr) => [acc,curr]);
   
   return (<React.Fragment>
     {partsJsx}
---
+++ part1/src/components/Part.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Part = (props) => {
+
+	return (<p>{props.name} {props.exerciseCount}</p>)
+}
+
+
+export default Part;
