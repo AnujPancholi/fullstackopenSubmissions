6400156026fe7ba279d3f4336de2a7f8ec177786
Anuj Pancholi <anuj.pancholi.1@gmail.com>
2020-05-24 16:42:30 +0530

    feat: SUBMISSION - 1.3 - add the objects for each part as instructed and refactor code to make it work

--- part1/src/App.js
+++ part1/src/App.js
@@ -9,23 +9,24 @@ import {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
-  const parts = [{
-    name: part1,
-    exerciseCount: exercises1
-  },{
-    name: part2,
-    exerciseCount: exercises2
-  },{
-    name: part3,
-    exerciseCount: exercises3
-  }];
+  const parts = [part1, part2, part3].map(part => ({
+    name: part.name,
+    exerciseCount: part.exercises
+  }));
 
   return (
     <div>
