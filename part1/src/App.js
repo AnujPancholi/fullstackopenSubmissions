b3b418d24e1f64857451e61ac7f05b5cc24a457b
Anuj Pancholi <anuj.pancholi.1@gmail.com>
2020-05-24 16:54:55 +0530

    feat: SUBMISSION - 1.4 - add parts array as instructed and refactored to make it work

--- part1/src/App.js
+++ part1/src/App.js
@@ -10,23 +10,25 @@ import {
 const App = () => {
   const course = 'Half Stack application development'
   
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
-  const parts = [part1, part2, part3].map(part => ({
-    name: part.name,
-    exerciseCount: part.exercises
-  }));
+  parts.forEach(part => {
+    part.exerciseCount = part.exercises;
+    delete part.exercises;
+  })
 
   return (
     <div>
