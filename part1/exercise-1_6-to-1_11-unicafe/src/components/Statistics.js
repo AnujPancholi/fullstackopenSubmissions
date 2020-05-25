3566de70acfaac31bbd6d35eccc6e78f7791bda3
Anuj Pancholi <anuj.pancholi.1@gmail.com>
2020-05-25 19:12:45 +0530

    feat: SUBMISSION - 1.10 - separate button and statistic component

--- part1/exercise-1_6-to-1_11-unicafe/src/components/Statistics.js
+++ part1/exercise-1_6-to-1_11-unicafe/src/components/Statistics.js
@@ -3,12 +3,14 @@ import TitleContainer from "./TitleContainer.js";
 
 
 const Statistic = ({name,stat}) => {
+	
   return (<React.Fragment>
       <p>{name}:&#9;{stat}</p>
     </React.Fragment>)
 }
 
 const Statistics = ({statList}) => {
+
   return (<div>
     <TitleContainer text="statistics" />
     {statList.some(item =>  !item.isCalculatedValue && item.stat>0) ? statList.map(item => (<Statistic name={item.name} stat={item.stat} />)).reduce((statsJsx,currentStatJsx) => [statsJsx,currentStatJsx]) : (<p>No feedback given</p>)}
